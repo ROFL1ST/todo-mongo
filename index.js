f0a5ce176516cd74f6268ca1f2896bef03e77334
ROFL1ST <danendrapr55@gmail.com>
2023-11-06 10:20:13 +0700

    P

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+const io = new Server(server, cors());
 mongoose
   .connect(uri)
   .then(() => {
